07869f76365833144d1bb8240da546798b8770eb
origi <xxjacobmartinezxx@gmail.com>
2024-03-28 19:43:31 -0700

    fixes

--- Current_Ast/CFileImp.cpp
+++ Current_Ast/CFileImp.cpp
@@ -1,3 +1,35 @@
-//
-// Created by origi on 3/26/2024.
-//
+/*
+    * Name: Jacob Martinez, 5007553403, 1003, ASSIGNMENT_6
+    * Description: DESCRIPTION_OF_PROGRAM
+    * Input: EXPECTED_PROGRAM_INPUT
+    * Output: EXPECTED_PROGRAM_OUTPUT
+*/
+
+#include "CFile.h"
+#include <iostream>
+
+using namespace std;
+
+void CFile::WriteToFirmFile(const CFirm& firm, const std::string& fileName)
+{
+    ofstream pFile(fileName);
+    pFile.open(fileName);
+    if(!pFile.is_open())
+    {
+        cout << "Error opening file" << endl;
+        return;
+    }
+    //iteratre over each CUser in firm.Users
+    for(int i = 0; i < firm.Users.size(); ++i)
+    {
+        pFile << firm.Users[i].getUserName() << endl;
+        //iterate over each COrder in CUser.Orders
+        for(int j = 0; j < firm.Users[i].Orders.size(); ++j)
+        {
+            pFile << firm.Users[i].Orders[j].getStockName() << " "
+                  << firm.Users[i].Orders[j].getPrice() << " "
+                  << firm.Users[i].Orders[j].getQuantity() << " "
+                  << firm.Users[i].Orders[j].getUser() << endl;
+        }
+    }
+}
--- Current_Ast/CFirmImp.cpp
+++ Current_Ast/CFirmImp.cpp
@@ -51,4 +51,62 @@ CFirm::CFirm()
      }
      return newFirm;
  }
- 
+
+ CFirm CFirm::operator+() const// unary operator; Deep Copy of the users Vector
+ {
+        CFirm newFirm;
+        for(int i=0; i < Users.size();++i)
+        {
+            newFirm.Users.push_back(Users[i]);
+        }
+        return newFirm;
+ }
+
+ bool CFirm::operator>(const CFirm& other) const
+ {
+     return portfolioValue > other.portfolioValue;
+ }
+
+ bool CFirm::operator<(const CFirm& other) const
+ {
+     return portfolioValue < other.portfolioValue;
+ }
+
+ CFirm& CFirm::operator++() //pre-increment operator
+ {
+    //this operator overloading increments the
+    // portfolio value of the CFirm object by 1
+    // and returns a reference to the updated CFirm object.
+    // overloaded pre-increment operator that adds a new CUser object to the users
+    //help?
+    CUser newUser;
+    Users.push_back(newUser);
+    return *this;
+ }
+
+ CFirm CFirm::operator++(int) //post-increment operator
+ {
+    //overloaded post-increment operator that adds a nw CUser object to the Users vector of the CFirm object and
+    // returns a copy of the original object
+    //confused
+    CUser newUser;
+    CFirm tempNew = *this;
+    Users.push_back(newUser);
+    return tempNew;
+ }
+
+ CFirm& CFirm::operator=(const CFirm& other) //Assignment operator
+    //overloaded alignment operator that copies the users of another CFIrm object to the current object.
+    {
+        Users = other.Users;
+        return *this;
+    }
+
+ ostream& operator<<(ostream& os, const CFirm& firm)
+ //Friend funciton that overloads the << operator to output the firm information icluding the names of it's users
+ {
+     os << "Portfolio Value: " << firm.portfolioValue << endl;
+     // how can i output the names of the users?
+     return os;
+ }
+
